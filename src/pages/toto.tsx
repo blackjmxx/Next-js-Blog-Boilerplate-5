97dcbfc09c28b08f62ed8cf303da1cd6370d8b93
blackjmxx <alandou.jeanmichel@gmail.com>
2022-02-03 03:35:11 +0100

    Update toto.tsx

--- src/pages/toto.tsx
+++ src/pages/toto.tsx
@@ -8,16 +8,10 @@ const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <Content>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-        recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
-        labore voluptatibus distinctio recusandae autem esse explicabo molestias
-        officia placeat, accusamus aut saepe.
+       toto
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-        recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
-        labore voluptatibus distinctio recusandae autem esse explicabo molestias
-        officia placeat, accusamus aut saepe.
+       toto
       </p>
     </Content>
   </Main>
